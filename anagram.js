c65cc186d58fd145c86ed342eb8e576d1d20aff5
Daknya Bam <daknyabam18@gmail.com>
2023-07-05 00:06:31 +0530

    anagrAM QUESTION

---
+++ anagram.js
@@ -0,0 +1,29 @@
+
+
+
+const anagram = (s, t) => {
+
+    console.log(s.length);
+    console.log(t.length);
+
+    if (s.length != t.length) {
+        return false;
+    }
+    var str1 = s.split("").sort().join("");
+    console.log(str1);
+    var str2 = t.split("").sort().join("");
+    console.log(str2);
+
+    if (str1 == str2) {
+        return true;
+    }
+    else {
+        return false;
+
+    }
+}
+
+var s = 'hello';
+var t = 'hello';
+
+console.log(anagram(s, t));
