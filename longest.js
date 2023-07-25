c8d8c259c07bb49974af3af677d8f8750bc829de
Daknya Bam <daknyabam18@gmail.com>
2023-07-25 23:54:59 +0530

    j

--- longest.js
+++ longest.js
@@ -4,21 +4,24 @@
  */
 var longestConsecutive = function (nums) {
     const newarr = new Set(nums);
+
     let longest = 0;
-
     for (let i = 0; i < nums.length; i++) {
-        // Only consider the starting element of a potential sequence
-        if (!newarr.has(nums[i - 1])) {
-            let length = 1;
+        console.log(i);
 
+        // Only consider the starting element of a potential sequence
+        if (!newarr.has(nums[i] - 1)) {
+            console.log(nums[i]);
+
+            let length = 1;
             // Check consecutive elements in the set
-            while (newarr.has(nums[i + length])) {
+            while (newarr.has(nums[i] + length)) {
+                console.log(i);
                 length++;
             }
             longest = Math.max(longest, length);
         }
     }
-
     return longest;
 };
 
