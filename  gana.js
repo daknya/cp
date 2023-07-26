6d70a37de41197efce5ae6ee4ba7b84e9b746075
Daknya Bam <daknyabam18@gmail.com>
2023-07-26 06:51:34 +0530

    test

---  gana.js
+++  gana.js
@@ -3,9 +3,16 @@ var groupAnagrams = function (strs) {
     let m = new Map();
     for (let str of strs) {
         let sorted = str.split("").sort().join("");
-        if (m.has(sorted)) m.set(sorted, [...m.get(sorted), str]);
-        else m.set(sorted, [str]);
+        if (m.has(sorted)) {
+            m.set(sorted, [...m.get(sorted), str]);
+        }
+        else {
+            console.log(sorted + " else");
+            m.set(sorted, [str]);
+        }
     }
     return Array.from(m.values());
 
 };
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
