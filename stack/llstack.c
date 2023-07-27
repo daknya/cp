46f0c73a2a5b682aa153f584eeaac46eac8f65a8
Daknya Bam <daknyabam18@gmail.com>
2023-07-27 16:25:54 +0530

    push and top completed

--- stack/llstack.c
+++ stack/llstack.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 // stack with linked list
 struct node
 {
@@ -10,8 +10,9 @@ struct node
 
 // global head
 struct node *head;
+struct node *top;
 
-void push(x)
+void push(int x)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
 
@@ -20,30 +21,54 @@ void push(x)
     {
         head = temp;
         temp->next = NULL;
+        top = temp;
     }
     else
     {
-        struct node *trav;
+        struct node *trav = head;
         while (trav->next != NULL)
         {
-            trav->next = temp;
-            temp->next = NULL;
+            trav = trav->next;
         }
+        trav->next = temp;
+
+        temp->next = NULL;
+        top = temp;
     };
 }
+
 void print()
 {
-    struct node *trav;
+    struct node *trav = head;
     while (trav->next != NULL)
     {
         printf("%d", trav->data);
+        trav = trav->next;
+    }
+    printf("%d", trav->data);
+}
+void getTop()
+{
+    if (top == NULL)
+    {
+        printf("empty stack");
+    }
+    else
+    {
+        printf("\n %d ", top->data);
     }
 }
 int main()
 {
     head = NULL;
-    printf("%p", head);
+
     // stack is a first in last out data structure
 
+    push(9);
+    push(8);
+    push(7);
+    push(6);
+    print();
+    getTop();
     return 0;
 }
