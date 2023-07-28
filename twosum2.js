d36d057311ab3aa886d7dabbdf9c5b7eb6f5fb60
Daknya Bam <daknyabam18@gmail.com>
2023-07-28 16:16:39 +0530

    corrected

---
+++ twosum2.js
@@ -0,0 +1,25 @@
+var arr = [2, 3, 4, 5, 6, 7];
+var target = 6;
+
+
+
+const twosumII = (arr, target) => {
+
+
+
+    var left = 0;
+    var right = arr.length - 1;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[left] + arr[right] == target) {
+            return [arr[left], arr[right]];
+        }
+        else if (arr[left] + arr[right] > target) {
+            right--;
+        }
+        else if (arr[left] + arr[right] < target) {
+            left++;
+        }
+    }
+
+}
+console.log(twosumII(arr, target)); 
