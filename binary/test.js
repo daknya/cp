5daaa8ca596700d60d05fcda1350abc8c8721896
Daknya Bam <daknyabam18@gmail.com>
2023-08-05 03:55:11 +0530

    day 5 not very consistance but ill make it

---
+++ binary/test.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    let l = 0;
+    let r = matrix[0].length;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][r - 1] >= target && matrix[i][l] <= target) {
+            while (l <= r) {
+                let mid = Math.floor((r + l) / 2);
+
+                if (matrix[i][mid] === target) {
+                    return true;
+                } else if (target > matrix[i][mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        } else {
+            return false;
+        }
+    }
+
+    return false;
+};
+
+console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3));
